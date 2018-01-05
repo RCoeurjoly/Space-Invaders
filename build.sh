2fb4aee58ecf90bc069d6e6e1b90876fc01e8f65
RCoeurjoly <rolandcoeurjoly@gmail.com>
2018-01-05 21:07:24 +0100

    Changed filename from vgaSI to vga

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-rm top.blif top.bin top.txt
-yosys -p "synth_ice40 -blif top.blif" top.v vgaSI.v formatVGA.v ship.v edgeDetectorDebounce.v timer.v 2>&1 | tee file.log
+yosys -p "synth_ice40 -blif top.blif" top.v vga.v formatVGA.v ship.v edgeDetectorDebounce.v timer.v 2>&1 | tee file.log
 arachne-pnr -d 1k -p vga.pcf top.blif -o top.txt 2>&1 | tee -a file.log
 icepack top.txt top.bin 2>&1 | tee -a file.log
 iceprog top.bin 2>&1 | tee -a file.log
-
+rm top.blif top.bin top.txt
