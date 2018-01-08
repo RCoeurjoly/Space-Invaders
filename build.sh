4f6516286b8a6f33d8273f472a298e1079d30f2e
RCoeurjoly <rolandcoeurjoly@gmail.com>
2018-01-08 22:32:15 +0100

    changed names of target to reflect naming

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-yosys -p "synth_ice40 -blif top.blif" top.v vga.v formatVGA.v ship.v edgeDetectorDebounce.v timer.v 2>&1 | tee file.log
-arachne-pnr -d 1k -p vga.pcf top.blif -o top.txt 2>&1 | tee -a file.log
-icepack top.txt top.bin 2>&1 | tee -a file.log
-iceprog top.bin 2>&1 | tee -a file.log
-rm top.blif top.bin top.txt
+yosys -p "synth_ice40 -blif space_invaders_top.blif" space_invader_top.v vga.v format_vga.v ship.v edge_detector_debounce.v timer_1us.v 2>&1 | tee file.log
+arachne-pnr -d 1k -p space_invader_top.pcf space_invader_top.blif -o space_invader_top.txt 2>&1 | tee -a file.log
+icepack space_invader_top.txt space_invader_top.bin 2>&1 | tee -a file.log
+iceprog space_invader_top.bin 2>&1 | tee -a file.log
+rm space_invader_top.blif space_invader_top.bin space_invader_top.txt
