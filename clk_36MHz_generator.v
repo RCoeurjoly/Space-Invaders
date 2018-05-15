9c7a8d78e9be62bf524ed0eab0e9360cb3110258
Alexandre Dumont <adumont@gmail.com>
2018-05-15 17:46:34 +0200

    Make  clk generator compatible for simulation

--- clk_36MHz_generator.v
+++ clk_36MHz_generator.v
@@ -5,17 +5,25 @@ module clk_36MHz_generator(
 	     input wire       clk_12MHz,
 	     output wire      clk_36MHz    
 	     );
-   
-   SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
-                   .PLLOUT_SELECT("GENCLK"),
-		   .DIVR(4'b0000),
-		   .DIVF(7'b0101111),
-		   .DIVQ(3'b100),
-		   .FILTER_RANGE(3'b001),
-		   ) uut (
-			  .REFERENCECLK(clk_12MHz),
-			  .PLLOUTCORE(clk_36MHz),
-			  .RESETB(1'b1),
-			  .BYPASS(1'b0)
-			  );
-   endmodule
+
+`ifndef SYNTHESIS
+    // SIMULATION
+        assign clk_36MHz = clk_12MHz;
+`else
+    // SYNTHESIS
+	SB_PLL40_CORE #(
+		.FEEDBACK_PATH("SIMPLE"),
+		.PLLOUT_SELECT("GENCLK"),
+		.DIVR(4'b0000),
+		.DIVF(7'b0101111),
+		.DIVQ(3'b100),
+		.FILTER_RANGE(3'b001),
+	) uut (
+		.REFERENCECLK(clk_12MHz),
+		.PLLOUTCORE(clk_36MHz),
+		.RESETB(1'b1),
+		.BYPASS(1'b0)
+	);
+`endif
+
+endmodule
